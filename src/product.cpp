71a8b77d1e020b5cb7efb925d20955440709d6d1
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2025-04-21 13:16:28 +0200

    Add product classes and tests

---
+++ src/product.cpp
@@ -0,0 +1,8 @@
+#include "product.hpp"
+
+std::shared_ptr<Product> Product::Basic(const quint64 price,
+                                        const QString name,
+                                        const QString description)
+{
+    return std::shared_ptr<Product>{new BasicProduct(price, name, description)};
+}
